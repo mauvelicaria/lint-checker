925791e499c5800a8a42070a2bafdd697425c103
Mau Velicaria <mauvelicaria@gmail.com>
2025-01-12 14:34:04 +0800

    fix eslint error

--- wp-content/plugins/hello-world-plugin/hello-world-plugin.ts
+++ wp-content/plugins/hello-world-plugin/hello-world-plugin.ts
@@ -1,6 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
   const helloDiv = document.createElement("div");
-  const unusedBool = true;
   helloDiv.id = "hello-world";
   helloDiv.innerText = "Hello, World!";
   document.body.appendChild(helloDiv);
