3a9af01e258ccc1502d5d0da439e4d20279b467c
Mau Velicaria <mauvelicaria@gmail.com>
2025-01-12 14:32:41 +0800

    test eslint error

--- wp-content/plugins/hello-world-plugin/hello-world-plugin.ts
+++ wp-content/plugins/hello-world-plugin/hello-world-plugin.ts
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   const helloDiv = document.createElement("div");
+  const unusedBool = true;
   helloDiv.id = "hello-world";
   helloDiv.innerText = "Hello, World!";
   document.body.appendChild(helloDiv);
